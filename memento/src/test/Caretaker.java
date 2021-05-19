747cd345e8fbac6b63fab990005444252c3ceab1
水魔王 <80053425+shuimowang@users.noreply.github.com>
2021-05-19 21:55:29 +0800

    备忘录模式

---
+++ memento/src/test/Caretaker.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Caretaker {
+	//负责人
+	private Memento memento;
+	public Memento getMemento()
+	{
+		return memento;
+	}
+	public void setMemento(Memento memento)
+	{
+		this.memento=memento;
+	}
+}
